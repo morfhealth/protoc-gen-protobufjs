1dc3468edccd43cd6f3a2cb0ca922fb028825e3f
James Thompson <james@morf.health>
2025-04-14 06:43:44 -0400

    Annotate only optional fields in msgs as $type | null | undefined

--- codegen.go
+++ codegen.go
@@ -257,7 +257,13 @@ func (c *Codegen) generate(file *descriptorpb.FileDescriptorProto, sourcePath []
 			fieldPath := append(messagePath, descriptorFieldTagNumber, int32(fieldIndex))
 
 			d.BlockComment(c.Comments[file.GetName()][fmt.Sprint(fieldPath)])
-			d.Lf("%s?: %s;", field.GetJsonName(), c.typeAnnotation(field))
+			if field.GetProto3Optional() ||
+				field.GetLabel() == descriptorpb.FieldDescriptorProto_LABEL_REPEATED ||
+				c.isMapField(field) {
+				d.Lf("%s?: %s;", field.GetJsonName(), c.typeAnnotation(field))
+			} else {
+				d.Lf("%s: %s;", field.GetJsonName(), c.typeAnnotation(field))
+			}
 		}
 		d.L("}")
 
@@ -875,10 +881,10 @@ func (c *Codegen) typeAnnotation(f *descriptorpb.FieldDescriptorProto) string {
 		entryType := c.Index.MessageTypes[f.GetTypeName()]
 		kf := entryType.GetField()[0]
 		vf := entryType.GetField()[1]
-		return fmt.Sprintf("Record<%s, %s>", c.presentSingularTypeAnnotation(kf), c.presentSingularTypeAnnotation(vf))
+		return fmt.Sprintf("Record<%s, %s>", c.singularTypeAnnotation(kf), c.singularTypeAnnotation(vf))
 	}
 	if f.GetLabel() == descriptorpb.FieldDescriptorProto_LABEL_REPEATED {
-		return fmt.Sprintf("Array<%s>", c.presentSingularTypeAnnotation(f))
+		return fmt.Sprintf("Array<%s>", c.singularTypeAnnotation(f))
 	}
 
 	return c.singularTypeAnnotation(f)
@@ -888,8 +894,6 @@ func (c *Codegen) singularTypeAnnotation(f *descriptorpb.FieldDescriptorProto) s
 	a := c.presentSingularTypeAnnotation(f)
 	if f.GetProto3Optional() {
 		a += " | null | undefined /* optional */"
-	} else if f.GetType() == descriptorpb.FieldDescriptorProto_TYPE_MESSAGE {
-		a += " | null | undefined"
 	}
 	return a
 }
